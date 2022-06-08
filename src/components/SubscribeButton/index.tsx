0795d0f00e0d338924ba4a76ad26c9b5b8ea4997
Jean Michel <jeaanmichel@protonmail.com>
2022-06-08 06:14:35 -0300

    created button component for subscribe

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./subscribebutton.module.scss";
+
+export function SubscribeButton() {
+  return (
+    <button type="button" className={styles.subscribeBtn}>
+      Subscribe now
+    </button>
+  );
+}
