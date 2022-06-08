fe805374df3721b3fa0046cea84b8963df8ffcec
Jean Michel <jeaanmichel@protonmail.com>
2022-06-08 05:55:35 -0300

    added styles for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import Head from "next/head";
 import type { NextPage } from "next";
 
+import styles from "./home.module.scss";
+
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1>Hello Next</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month.</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   );
 };
