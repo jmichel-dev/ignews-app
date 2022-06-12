f4431cbad8280101f10cf244b43c4b9550c9806c
Jean Michel <jeaanmichel@protonmail.com>
2022-06-12 19:38:11 -0300

    added revalidate to stattic props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import type { GetServerSideProps, NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
@@ -36,7 +36,7 @@ const Home: NextPage<HomeProps> = ({ product }) => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1L8Kp0JOrUJgFJ4UAJRG9lih", {
     expand: ["product"],
   });
@@ -53,6 +53,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 horas
   };
 };
 
