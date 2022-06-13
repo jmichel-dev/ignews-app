eac9712de70474263980a482da50037d8ac7627a
Jean Michel <jeaanmichel@protonmail.com>
2022-06-12 22:37:20 -0300

    adding github user info to the database after the signin

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
+import { query as q } from "faunadb";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,6 +12,29 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: { params: { scope: "read:user" } },
     }),
-    // ...add more providers here
   ],
+  secret: process.env.SECRET,
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email, name, image } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email!))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email, name, image } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email!)))
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
