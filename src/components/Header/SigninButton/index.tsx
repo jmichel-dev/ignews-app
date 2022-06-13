5e66d3a82301b7d9c6ea1740e6e9db1ac56c2410
Jean Michel <jeaanmichel@protonmail.com>
2022-06-12 21:57:38 -0300

    defined authentication using github

--- src/components/Header/SigninButton/index.tsx
+++ src/components/Header/SigninButton/index.tsx
@@ -1,22 +1,23 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./signinbutton.module.scss";
 
 export function SigninButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   let button = (
-    <button className={styles.signinButton}>
+    <button className={styles.signinButton} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      Jean Michel
+      {session?.user?.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   );
 
-  if (!isUserLoggedIn) {
+  if (!session) {
     button = (
-      <button className={styles.signinButton}>
+      <button className={styles.signinButton} onClick={() => signIn("github")}>
         <FaGithub color="#eba417" />
         Sign in with Github
       </button>
